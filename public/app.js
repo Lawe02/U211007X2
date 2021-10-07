081cb3d89f1ab8adbf394508c77bd1ae04e4df18
Lawe Zangena <lawza02@gmail.com>
2021-10-07 16:57:43 +0200

    modified app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-console.log('i am also alive');
+ let dataURL = '../data/exercise1.json';
+ let req = new XMLHttpRequest();
+req.open('GET', dataURL); 
+req.responseType='json'; 
+req.send(); 
+
+req.onload = () => {
+    const data = req.response;
+    console.log(data);
+}
