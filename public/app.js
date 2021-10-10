5e765741c7b0e45d947151f935246e9db6baff26
Lawe Zangena <lawza02@gmail.com>
2021-10-11 01:25:14 +0200

    now you can hide and show duck

--- public/app.js
+++ public/app.js
@@ -13,15 +13,35 @@ req.onload = () => {
     const elementH1 = document.createElement('h1');
     const elementP = document .createElement('p');
     const elementImg = document.createElement('img');
+    const elementBtn = document.createElement('button');
     elementImg.id  = 'imgId';
+    elementBtn.id = 'btnId';
     
     elementH1.textContent = data.headline;
     elementP.textContent = data.intro;
     elementImg.setAttribute('src', data.articleImage);
+    elementBtn.textContent = "Hide";
+
+    elementBtn.addEventListener('click', ButtonHandler);
 
     document.getElementById('Exercise1').appendChild(elementH1);
     document.getElementById('Exercise1').appendChild(elementP);
     document.getElementById('Exercise1').appendChild(elementImg);
+    document.getElementById('Exercise1').appendChild(elementBtn);
 
+};
+
+ const ButtonHandler = () => {
+    const elementImg = document.getElementById('imgId');
+    const elementBtn = document.getElementById('btnId')
+
+    if (elementImg.style.display == 'none'){
+        elementImg.style.display = 'block';
+        elementBtn.textContent = 'Hide';
+    } else {
+        elementImg.style.display = 'none';
+        elementBtn.textContent = 'Show';
+
+    }
 
 }
